4d25248689aacacc6f3a912488050699f8f1757a
kylin <luqilin1234@126.com>
2017-06-09 20:53:12 +0800

    add cmd console

--- MyClient/frameworks/runtime-src/proj.win32/main.cpp
+++ MyClient/frameworks/runtime-src/proj.win32/main.cpp
@@ -2,13 +2,28 @@
 #include "SimulatorWin.h"
 #include <shellapi.h>
 
+// uncomment below line, open debug console
+#define USE_WIN32_CONSOLE
+
 int WINAPI _tWinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPTSTR    lpCmdLine,
 	int       nCmdShow)
 {
+#ifdef USE_WIN32_CONSOLE
+    AllocConsole();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+#endif
+
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
     auto simulator = SimulatorWin::getInstance();
-    return simulator->run();
+    int ret = simulator->run();
+	
+#ifdef USE_WIN32_CONSOLE
+    FreeConsole();
+#endif
+	return ret;
 }
