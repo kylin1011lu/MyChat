e418f3eeff35080e169991ca9a343fd0d9103c99
kylin <luqilin1234@126.com>
2017-06-16 19:18:38 +0800

    update android app icon

--- MyClient/src/Definition/ConnectDef.lua
+++ MyClient/src/Definition/ConnectDef.lua
@@ -7,15 +7,16 @@ cc.exports.Def = {}
 --虚拟机ip
 -- Def.ServerAddress = "192.168.248.128"
 --本机
-if TARGETPLATFORM == cc.PLATFORM_OS_ANDROID then
+if TARGETPLATFORM == cc.PLATFORM_OS_ANDROID or TARGETPLATFORM == cc.PLATFORM_OS_IPAD
+	or TARGETPLATFORM == cc.PLATFORM_OS_IPHONE then
 	--Def.ServerAddress = "10.0.2.6"
 	--阿里云服务器
 	Def.ServerAddress="59.110.213.172"
 else
-	--Def.ServerAddress = "127.0.0.1"
+	Def.ServerAddress = "127.0.0.1"
 
 	--阿里云服务器
-	Def.ServerAddress="59.110.213.172"
+	--Def.ServerAddress="59.110.213.172"
 end
 
 Def.ServerPort=12012
